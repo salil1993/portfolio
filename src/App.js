f306a23c063d1fc110be8032f32c04e1111dc2af
Salil Samdarshy <52833987+salil1993@users.noreply.github.com>
2025-10-05 11:54:37 +0530

    Fix CSS import case sensitivity issue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import ReactGA from "react-ga4";
-
 import Homepage from "./pages/homepage";
 import About from "./pages/about";
 import Projects from "./pages/projects";
@@ -9,9 +8,8 @@ import Articles from "./pages/articles";
 import ReadArticle from "./pages/readArticle";
 import Contact from "./pages/contact";
 import Notfound from "./pages/404";
-
 import { TRACKING_ID } from "./data/tracking";
-import "./app.css";
+import "./App.css";
 
 function App() {
 	useEffect(() => {
