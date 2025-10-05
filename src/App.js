f1090fba474ebc14aba75685859266346d7fdb18
Salil Samdarshy <52833987+salil1993@users.noreply.github.com>
2025-10-05 12:19:00 +0530

    fix: use HashRouter for GitHub Pages compatibility

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { BrowserRouter,Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import ReactGA from "react-ga4";
 import Homepage from "./pages/homepage";
 import About from "./pages/about";
@@ -20,7 +20,7 @@ function App() {
 
 	return (
 		<div className="App">
-		<BrowserRouter>
+		<HashRouter>
 			<Routes>
 				<Route path="/" element={<Homepage />} />
 				<Route path="/about" element={<About />} />
@@ -30,7 +30,7 @@ function App() {
 				<Route path="/contact" element={<Contact />} />
 				<Route path="*" element={<Notfound />} />
 			</Routes>
-	</BrowserRouter>
+	</HashRouter>
 		</div>
 	);
 }
